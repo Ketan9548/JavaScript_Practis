47a9237eb4e4e9b1e783c5c26402a0a577d65f4e
Ketan9548 <kc918062@gmail.com>
2024-10-16 16:01:19 +0530

    js practies

---
+++ Baisc/Comparison.js
@@ -0,0 +1,11 @@
+// console.log(4==2);
+// console.log(3===3);
+// console.log(2>=2);
+// console.log(4<=2);
+
+// console.log("2">1);
+// console.log("0"==0);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >=0);
---
+++ Baisc/datatype_summery.js
@@ -0,0 +1,8 @@
+// primative 
+
+// 7 type = String , Number , Boolean , null, undefined, Symbol
+
+// Js is Dynamic Type Language , not declayer the data type in any value
+
+// refrence type (Non Primative )
+// Array , Objects , Functinon
