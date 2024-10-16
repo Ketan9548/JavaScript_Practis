5dcd6a3ef5af252756cc00e64539aaafd5d6f91f
Ketan9548 <kc918062@gmail.com>
2024-10-16 16:07:41 +0530

    change

--- Baisc/basic_operation.js
+++ Baisc/basic_operation.js
@@ -1,4 +1,30 @@
-let val = 3
-let val1 = 9
+// let val = 3
+// let val1 = 9
 
-console.log(val+val1);
+// console.log(val+val1);
+
+// for (let i = 0; i < 5; i++) {
+//     setTimeout(()=>{
+//         console.log(i)
+//     },)
+// }
+
+// output
+// 0
+// 1
+// 2
+// 3
+// 4
+
+for (var i = 0; i < 5; i++) {
+    setTimeout(()=>{
+        console.log(i)
+    },)
+}
+
+// output
+// 5
+// 5
+// 5
+// 5
+// 5
